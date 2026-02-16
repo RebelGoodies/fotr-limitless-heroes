8716c68ba9ced35846d7327ba9b4dbafbb038810
rebelgoodies <rebelgoodies@users.noreply.github.com>
2026-02-16 14:14:29 -0700

    style: replace tabs with spaces

--- mod/Data/Scripts/Library/eawx-plugins/event-handler/PreFTGU.lua
+++ mod/Data/Scripts/Library/eawx-plugins/event-handler/PreFTGU.lua
@@ -20,80 +20,80 @@ function EventManagerExtra:new(galactic_conquest, human_player, planets, id)
     self.galactic_conquest = galactic_conquest
     self.Active_Planets = StoryUtil.GetSafePlanetTable()
 
-	self.entry_time = GetCurrentTime()
-	self.EventsFired = false
-	self.spawn_heroes = true
+    self.entry_time = GetCurrentTime()
+    self.EventsFired = false
+    self.spawn_heroes = true
 
-	if id == "CUSTOM" then
-		self.spawn_heroes = false
-	end
+    if id == "CUSTOM" then
+        self.spawn_heroes = false
+    end
 
-	self.CISGrievousShipHandler = CISGrievousShipEvent(self.galactic_conquest)
-	self.CISMandaloreSupport = CISMandaloreSupportEvent(self.galactic_conquest, self.Active_Planets["MANDALORE"])
-	self.CISHoldouts = CISHoldoutsEvent(self.galactic_conquest)
+    self.CISGrievousShipHandler = CISGrievousShipEvent(self.galactic_conquest)
+    self.CISMandaloreSupport = CISMandaloreSupportEvent(self.galactic_conquest, self.Active_Planets["MANDALORE"])
+    self.CISHoldouts = CISHoldoutsEvent(self.galactic_conquest)
 
-	crossplot:subscribe("INITIALIZE_AI", self.init_events, self)
-	crossplot:subscribe("STARTING_SIZE_PICK", self.check_spawn_heroes, self) --For Custom GC
-	crossplot:subscribe("HERO_PICK", self.heroes_spawned_already, self)
+    crossplot:subscribe("INITIALIZE_AI", self.init_events, self)
+    crossplot:subscribe("STARTING_SIZE_PICK", self.check_spawn_heroes, self) --For Custom GC
+    crossplot:subscribe("HERO_PICK", self.heroes_spawned_already, self)
 end
 
 function EventManagerExtra:init_events()
-	if not self.EventsFired then
-		-- crossplot:publish("COMMAND_STAFF_DECREMENT", {-2,0,-2,-2,-1,-1}, 0)
-		if self.spawn_heroes then
-			self:spawn_FTGU_heroes()
-		end
-		self:hero_era_unlocks()
-		crossplot:publish("COMMAND_STAFF_INITIALIZE", nil)
-		self.EventsFired = true
-	end
+    if not self.EventsFired then
+        -- crossplot:publish("COMMAND_STAFF_DECREMENT", {-2,0,-2,-2,-1,-1}, 0)
+        if self.spawn_heroes then
+            self:spawn_FTGU_heroes()
+        end
+        self:hero_era_unlocks()
+        crossplot:publish("COMMAND_STAFF_INITIALIZE", nil)
+        self.EventsFired = true
+    end
 end
 
 function EventManagerExtra:spawn_FTGU_heroes()
-	local path = "eawx-mod-fotr/spawn-sets/"
-	local sets = {"EraOneStartSet", "EraTwoStartSet", "EraThreeStartSet", "EraFourStartSet", "EraFiveStartSet"}
+    local path = "eawx-mod-fotr/spawn-sets/"
+    local sets = {"EraOneStartSet", "EraTwoStartSet", "EraThreeStartSet", "EraFourStartSet", "EraFiveStartSet"}
 
-	---@type integer
-	local era = GlobalValue.Get("CURRENT_ERA")
+    ---@type integer
+    local era = GlobalValue.Get("CURRENT_ERA")
 
-	if not era or not sets[era] then
-		return
-	end
+    if not era or not sets[era] then
+        return
+    end
 
-	local success, starting_spawns = pcall(require, path.."FTGU_"..sets[era])
-	if not success then
-		StoryUtil.ShowScreenText("WARNING: Missing or invalid FTGU_"..sets[era]..". Using backup instead.", 5, nil, {r = 220, g = 220, b = 0})
-		success, starting_spawns = pcall(require, path..sets[era])
-	end
-	if not success then
-		return
-	end
+    local success, starting_spawns = pcall(require, path.."FTGU_"..sets[era])
+    if not success then
+        StoryUtil.ShowScreenText("WARNING: Missing or invalid FTGU_"..sets[era]..". Using backup instead.", 5, nil, {r = 220, g = 220, b = 0})
+        success, starting_spawns = pcall(require, path..sets[era])
+    end
+    if not success then
+        return
+    end
 
-	StoryUtil.ShowScreenText("Spawning heroes", 5, nil, {r = 0, g = 180, b = 0})
-	for faction, spawnlist in pairs(starting_spawns) do
-		for planet, herolist in pairs(spawnlist) do
-			StoryUtil.SpawnAtSafePlanet(planet, Find_Player(faction), self.Active_Planets, herolist)
-		end
-	end
+    StoryUtil.ShowScreenText("Spawning heroes", 5, nil, {r = 0, g = 180, b = 0})
+    for faction, spawnlist in pairs(starting_spawns) do
+        for planet, herolist in pairs(spawnlist) do
+            StoryUtil.SpawnAtSafePlanet(planet, Find_Player(faction), self.Active_Planets, herolist)
+        end
+    end
 end
 
 function EventManagerExtra:hero_era_unlocks()
-	local era = GlobalValue.Get("CURRENT_ERA")
-	if era == 1 then
-		crossplot:publish("REPUBLIC_ADMIRAL_DECREMENT", 99, 4) --Clone Officers
-		crossplot:publish("REPUBLIC_ADMIRAL_DECREMENT", 99, 5) --Clone Commandos
-	end
+    local era = GlobalValue.Get("CURRENT_ERA")
+    if era == 1 then
+        crossplot:publish("REPUBLIC_ADMIRAL_DECREMENT", 99, 4) --Clone Officers
+        crossplot:publish("REPUBLIC_ADMIRAL_DECREMENT", 99, 5) --Clone Commandos
+    end
 end
 
 ---@param choice string
 function EventManagerExtra:check_spawn_heroes(choice)
-	if choice == "CUSTOM_GC_SMALL_START" then
-		self.spawn_heroes = false
-	end
+    if choice == "CUSTOM_GC_SMALL_START" then
+        self.spawn_heroes = false
+    end
 end
 
 function EventManagerExtra:heroes_spawned_already()
-	self.spawn_heroes = false
+    self.spawn_heroes = false
 end
 
 return EventManagerExtra
--- mod/Data/Scripts/Library/eawx-plugins/republic-heroes/HeroesManager.lua
+++ mod/Data/Scripts/Library/eawx-plugins/republic-heroes/HeroesManager.lua
@@ -10,7 +10,6 @@ require("eawx-util/UnitUtil")
 require("HeroSystem")
 require("HeroSystem2")
 
-
 ---@class HeroesManager
 HeroesManager = class()
 
@@ -19,149 +18,149 @@ HeroesManager = class()
 ---@param id string Name of the gc "PROGRESSIVE", "FTGU", ect.
 ---@param hero_clones_p2_disabled boolean If phase II clones should auto activate on high enough starting era.
 function HeroesManager:new(gc, id, hero_clones_p2_disabled)
-	gc.Events.GalacticProductionFinished:attach_listener(self.on_production_finished, self)
-	gc.Events.GalacticHeroKilled:attach_listener(self.on_galactic_hero_killed, self)
-	self.id = id
-	self.human_player = gc.HumanPlayer
-	StoryUtil.ShowScreenText("ID: "..tostring(id), 5, nil, {r = 102, g = 102, b = 102})
-	
-	--self.HuttHeroes = HuttHeroes(gc, id)
-	self.CISHeroes = CISHeroes(gc, id)
-	self.RepHeroes = RepublicHeroes(gc, gc.Events.GalacticHeroKilled, gc.HumanPlayer, hero_clones_p2_disabled, id)
+    gc.Events.GalacticProductionFinished:attach_listener(self.on_production_finished, self)
+    gc.Events.GalacticHeroKilled:attach_listener(self.on_galactic_hero_killed, self)
+    self.id = id
+    self.human_player = gc.HumanPlayer
+    StoryUtil.ShowScreenText("ID: "..tostring(id), 5, nil, {r = 102, g = 102, b = 102})
 
-	self.lorz_entry = {"LORZ_ASSIGN",{"LORZ_RETIRE"},{"LORZ_GEPTUN"},"Lorz Geptun", ["Companies"] = {"LORZ_GEPTUN_TEAM"}}
-	
-	GlobalValue.Set("MANDO_DEFAULT", 1)
-	self.MandoSkins = {
-		"Default Mandalorian armour reset",	
-		"Default Mandalorian armour set to Protectors",
-		"Default Mandalorian armour set to Death Watch",
-		"Default Mandalorian armour set to Maul",
-	}
-	local tech = Find_Object_Type("OPTION_CYCLE_MANDO")
-	if TestValid(tech) then
-		self.human_player.Unlock_Tech(tech)
-	end
+    --self.HuttHeroes = HuttHeroes(gc, id)
+    self.CISHeroes = CISHeroes(gc, id)
+    self.RepHeroes = RepublicHeroes(gc, gc.Events.GalacticHeroKilled, gc.HumanPlayer, hero_clones_p2_disabled, id)
+
+    self.lorz_entry = {"LORZ_ASSIGN",{"LORZ_RETIRE"},{"LORZ_GEPTUN"},"Lorz Geptun", ["Companies"] = {"LORZ_GEPTUN_TEAM"}}
+
+    GlobalValue.Set("MANDO_DEFAULT", 1)
+    self.MandoSkins = {
+        "Default Mandalorian armour reset",
+        "Default Mandalorian armour set to Protectors",
+        "Default Mandalorian armour set to Death Watch",
+        "Default Mandalorian armour set to Maul",
+    }
+    local tech = Find_Object_Type("OPTION_CYCLE_MANDO")
+    if TestValid(tech) then
+        self.human_player.Unlock_Tech(tech)
+    end
 end
 
 ---Extra setup once the other hero systems are initiated.
 function HeroesManager:init()
-	--Logger:trace("entering HeroesManager:init")
-	if self.inited or not self.RepHeroes.CommandStaff_Initialized or not self.CISHeroes.CommandStaff_Initialized then
-		return
-	end
-	self.inited = true
+    --Logger:trace("entering HeroesManager:init")
+    if self.inited or not self.RepHeroes.CommandStaff_Initialized or not self.CISHeroes.CommandStaff_Initialized then
+        return
+    end
+    self.inited = true
 
-	--[[
-	validate_hero_data_table(admiral_data)
-	validate_hero_data_table(moff_data)
-	validate_hero_data_table(council_data)
-	validate_hero_data_table(clone_data)
-	validate_hero_data_table(commando_data)
-	validate_hero_data_table(general_data)
-	validate_hero_data_table(senator_data)
-	validate_hero_data_table(space_data)
-	validate_hero_data_table(ground_data)
-	validate_hero_data_table(sith_data)
-	--]]
-	
-	local tech_level = GlobalValue.Get("CURRENT_ERA")
-	
-	if tech_level == 2 and ground_data then -- For CIS
-		ground_data.full_list["Lorz"] = self.lorz_entry
-		Handle_Hero_Add_2("Lorz", ground_data)
-	end
-	if tech_level >= 3 and general_data then -- For Republic
-		general_data.full_list["Lorz"] = self.lorz_entry
-		Handle_Hero_Add_2("Lorz", general_data)
-	end
-	
-	if self.id == "PROGRESSIVE" or self.id == "FTGU" or self.id == "CUSTOM" then
-		if tech_level == 2 or tech_level == 3 then
-			UnitUtil.SetLockList("REBEL", {"MAD_CLONE_MUNIFICENT"}, true)
-		end
-		if tech_level == 4 then
-			UnitUtil.SetLockList("REBEL", {"VENATOR_RENOWN"}, true)
-		end
+    --[[
+    validate_hero_data_table(admiral_data)
+    validate_hero_data_table(moff_data)
+    validate_hero_data_table(council_data)
+    validate_hero_data_table(clone_data)
+    validate_hero_data_table(commando_data)
+    validate_hero_data_table(general_data)
+    validate_hero_data_table(senator_data)
+    validate_hero_data_table(space_data)
+    validate_hero_data_table(ground_data)
+    validate_hero_data_table(sith_data)
+    --]]
 
-		local ind_planet = FindPlanet("BPFASSH")
-		if not ind_planet then
-			ind_planet = StoryUtil.FindFriendlyPlanet("Independent_Forces", false)
-		end
-		if ind_planet and Find_Object_Type("Dark_Jedi_Headmaster_Company") then
-			StoryUtil.SpawnAtSafePlanet(ind_planet.Get_Type().Get_Name(), ind_planet.Get_Owner(), StoryUtil.GetSafePlanetTable(), {"Dark_Jedi_Headmaster_Company"}, false, true)
-		end
-	else --Historical
-		local systems = {admiral_data, moff_data, council_data, general_data, senator_data, space_data, ground_data, sith_data}
-		for _, hero_data in pairs(systems) do
-			lock_retires_if_on_map(hero_data)
-		end
-		UnitUtil.SetLockList("EMPIRE", {"DOMINO_SQUAD_TEAM"}, false)
-	end
+    local tech_level = GlobalValue.Get("CURRENT_ERA")
+
+    if tech_level == 2 and ground_data then -- For CIS
+        ground_data.full_list["Lorz"] = self.lorz_entry
+        Handle_Hero_Add_2("Lorz", ground_data)
+    end
+    if tech_level >= 3 and general_data then -- For Republic
+        general_data.full_list["Lorz"] = self.lorz_entry
+        Handle_Hero_Add_2("Lorz", general_data)
+    end
+
+    if self.id == "PROGRESSIVE" or self.id == "FTGU" or self.id == "CUSTOM" then
+        if tech_level == 2 or tech_level == 3 then
+            UnitUtil.SetLockList("REBEL", {"MAD_CLONE_MUNIFICENT"}, true)
+        end
+        if tech_level == 4 then
+            UnitUtil.SetLockList("REBEL", {"VENATOR_RENOWN"}, true)
+        end
+
+        local ind_planet = FindPlanet("BPFASSH")
+        if not ind_planet then
+            ind_planet = StoryUtil.FindFriendlyPlanet("Independent_Forces", false)
+        end
+        if ind_planet and Find_Object_Type("Dark_Jedi_Headmaster_Company") then
+            StoryUtil.SpawnAtSafePlanet(ind_planet.Get_Type().Get_Name(), ind_planet.Get_Owner(), StoryUtil.GetSafePlanetTable(), {"Dark_Jedi_Headmaster_Company"}, false, true)
+        end
+    else --Historical
+        local systems = {admiral_data, moff_data, council_data, general_data, senator_data, space_data, ground_data, sith_data}
+        for _, hero_data in pairs(systems) do
+            lock_retires_if_on_map(hero_data)
+        end
+        UnitUtil.SetLockList("EMPIRE", {"DOMINO_SQUAD_TEAM"}, false)
+    end
 end
 
 ---Using experimental protected calls to prevent framework crash
 ---@param planet Planet
 ---@param object_type_name string
 function HeroesManager:on_production_finished(planet, object_type_name)
-	--Logger:trace("entering HeroesManager:on_production_finished")
-	local success1, err1 = pcall(function()
-		self.RepHeroes:on_production_finished(planet, object_type_name)
-	end)
-	if not success1 then
-		StoryUtil.ShowScreenText("===== RepHeroes:on_production_finished ===== ", 30, nil, {r = 244, g = 200, b = 0})
-		StoryUtil.ShowScreenText("Please Report Bug for Limitless Heroes:\n"..err1, 30, nil, {r = 244, g = 150, b = 0})
-	end
+    --Logger:trace("entering HeroesManager:on_production_finished")
+    local success1, err1 = pcall(function()
+        self.RepHeroes:on_production_finished(planet, object_type_name)
+    end)
+    if not success1 then
+        StoryUtil.ShowScreenText("===== RepHeroes:on_production_finished ===== ", 30, nil, {r = 244, g = 200, b = 0})
+        StoryUtil.ShowScreenText("Please Report Bug for Limitless Heroes:\n"..err1, 30, nil, {r = 244, g = 150, b = 0})
+    end
 
-	local success2, err2 = pcall(function()
-		self.CISHeroes:on_production_finished(planet, object_type_name)
-	end)
-	if not success2 then
-		StoryUtil.ShowScreenText("===== CISHeroes:on_production_finished ===== ", 30, nil, {r = 244, g = 200, b = 0})
-		StoryUtil.ShowScreenText("Please Report Bug for Limitless Heroes:\n"..err2, 30, nil, {r = 244, g = 150, b = 0})
-	end
+    local success2, err2 = pcall(function()
+        self.CISHeroes:on_production_finished(planet, object_type_name)
+    end)
+    if not success2 then
+        StoryUtil.ShowScreenText("===== CISHeroes:on_production_finished ===== ", 30, nil, {r = 244, g = 200, b = 0})
+        StoryUtil.ShowScreenText("Please Report Bug for Limitless Heroes:\n"..err2, 30, nil, {r = 244, g = 150, b = 0})
+    end
 
-	if success1 and success2 then
-		self:init()
-	end
-	
-	if object_type_name == "OPTION_CYCLE_MANDO" then
-		self:Option_Cycle_Mando_Colour()
-	end
+    if success1 and success2 then
+        self:init()
+    end
+
+    if object_type_name == "OPTION_CYCLE_MANDO" then
+        self:Option_Cycle_Mando_Colour()
+    end
 end
 
 ---Using experimental protected calls to prevent framework crash
 ---@param hero_name string Upper case XML hero team name.
 ---@param owner string Upper case faction name.
 function HeroesManager:on_galactic_hero_killed(hero_name, owner)
-	--Logger:trace("entering HeroesManager:on_galactic_hero_killed")
-	
-	local success1, err1 = pcall(function()
-		self.RepHeroes:on_galactic_hero_killed(hero_name, owner)
-	end)
-	if not success1 then
-		StoryUtil.ShowScreenText("===== RepHeroes:on_galactic_hero_killed ===== ", 30, nil, {r = 244, g = 200, b = 0})
-		StoryUtil.ShowScreenText("Please Report Bug for Limitless Heroes:\n"..err1, 30, nil, {r = 244, g = 150, b = 0})
-	end
+    --Logger:trace("entering HeroesManager:on_galactic_hero_killed")
 
-	local success2, err2 = pcall(function()
-		self.CISHeroes:on_galactic_hero_killed(hero_name, owner)
-	end)
-	if not success2 then
-		StoryUtil.ShowScreenText("===== CISHeroes:on_galactic_hero_killed ===== ", 30, nil, {r = 244, g = 200, b = 0})
-		StoryUtil.ShowScreenText("Please Report Bug for Limitless Heroes:\n"..err2, 30, nil, {r = 244, g = 150, b = 0})
-	end
+    local success1, err1 = pcall(function()
+        self.RepHeroes:on_galactic_hero_killed(hero_name, owner)
+    end)
+    if not success1 then
+        StoryUtil.ShowScreenText("===== RepHeroes:on_galactic_hero_killed ===== ", 30, nil, {r = 244, g = 200, b = 0})
+        StoryUtil.ShowScreenText("Please Report Bug for Limitless Heroes:\n"..err1, 30, nil, {r = 244, g = 150, b = 0})
+    end
+
+    local success2, err2 = pcall(function()
+        self.CISHeroes:on_galactic_hero_killed(hero_name, owner)
+    end)
+    if not success2 then
+        StoryUtil.ShowScreenText("===== CISHeroes:on_galactic_hero_killed ===== ", 30, nil, {r = 244, g = 200, b = 0})
+        StoryUtil.ShowScreenText("Please Report Bug for Limitless Heroes:\n"..err2, 30, nil, {r = 244, g = 150, b = 0})
+    end
 end
 
 ---Globally changes the default mandalorian soldier skin used by all factions.
 function HeroesManager:Option_Cycle_Mando_Colour()
-	--Logger:trace("entering HeroesManager:Option_Cycle_Mando_Colour")
+    --Logger:trace("entering HeroesManager:Option_Cycle_Mando_Colour")
 
-	local mando_skin = GlobalValue.Get("MANDO_DEFAULT")
-	mando_skin = mando_skin + 1
-	if mando_skin > 4 then
-		mando_skin = 1
-	end
-	GlobalValue.Set("MANDO_DEFAULT", mando_skin)
-	StoryUtil.ShowScreenText(self.MandoSkins[mando_skin], 5, nil, {r = 244, g = 200, b = 0})
+    local mando_skin = GlobalValue.Get("MANDO_DEFAULT")
+    mando_skin = mando_skin + 1
+    if mando_skin > 4 then
+        mando_skin = 1
+    end
+    GlobalValue.Set("MANDO_DEFAULT", mando_skin)
+    StoryUtil.ShowScreenText(self.MandoSkins[mando_skin], 5, nil, {r = 244, g = 200, b = 0})
 end
